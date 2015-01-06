9126aafc49e582afe1f7dc412dc9a7ada1bd9d6e
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2015-01-06 14:42:30 -0600

    new maitenance page :)

---
+++ templates/maintenance-page.tpl.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file
+ * Implementation to display a single Drupal page while offline.
+ *
+ * All the available variables are mirrored in page.tpl.php.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_maintenance_page()
+ * @see bartik_process_maintenance_page()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <link rel="stylesheet" type="text/css" href="http://knowpulse2.usask.ca/portal/sites/all/themes/kptheme/css/maintenance-page.css">
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <div id="page-wrapper"><div id="page">
+
+  <div id="header" class="with-secondary-menu"><div class="section clearfix">
+
+        <img src="http://knowpulse2.usask.ca/portal/sites/default/files/color/kptheme-61b1ff63/logo.png" alt="Home" />
+    
+          <div id="name-and-slogan">
+
+                              <div id="site-name" class="element-invisible">
+              <strong>
+                <a href="/kp-dev/" title="Home" rel="home"><span>KnowPulse</span></a>
+              </strong>
+            </div>
+                  
+        
+      </div> <!-- /#name-and-slogan -->
+  </div></div> <!-- /.section, /#header -->
+
+    <div id="main-wrapper"><div id="main" class="clearfix">
+      <div id="content" class="column"><div class="section">
+        <a id="main-content"></a>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        
+        <!-- Specific Friendly content depending on error -->
+        <?php if (preg_match('/remaining connection slots are reserved/',$messages)): ?>
+          <p>KnowPulse is experiencing higher than expected traffic. Please try back later.</p>
+        <?php else: ?>
+          <?php print $content; ?>
+        <?php endif; ?>
+
+        <!-- Original Error for administrator -->
+        <?php if ($messages): ?>
+          <div id="messages"><div class="section clearfix">
+            <?php print $messages; ?>
+          </div></div> <!-- /.section, /#messages -->
+        <?php endif; ?>
+      </div></div> <!-- /.section, /#content -->
+    </div></div> <!-- /#main, /#main-wrapper -->
+
+  </div></div> <!-- /#page, /#page-wrapper -->
+</body>
+</html>
