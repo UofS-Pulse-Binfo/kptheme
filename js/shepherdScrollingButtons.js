ca9f0b71743440280291f8768e97f65c87229e31
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2016-02-10 16:48:57 -0600

    Fixed shepherd scrolling buttons to handle missing steps.

--- js/shepherdScrollingButtons.js
+++ js/shepherdScrollingButtons.js
@@ -3,35 +3,52 @@ Drupal.kptheme = {
   shepherd: {
     scrollingButtons: function(prevStepSelector, nextStepSelector, isLastStep) {
       isLastStep = isLastStep || false;
-      secondButton = 'Next';
-      secondButtonClass = 'shepherd-button-primary';
-      if (isLastStep) { secondButton = 'Done';  secondButtonClass = 'shepherd-button-complete'; }
-      return [
-          {
-            text: 'Back',
-            action: function() {
-                // Move on to the next step.
-                tour.back();
-                // Now, scroll to the next step.
-                $('html, body').animate({
-                  scrollTop: $(prevStepSelector).offset().top-100
-                }, 1000);
-              },
-            classes: 'shepherd-button-secondary'
-          },
-          {
-              text: secondButton,
-              action: function() {
-                // Move on to the next step.
-                tour.next();
-                // Now, scroll to the next step.
-                $('html, body').animate({
-                  scrollTop: $(nextStepSelector).offset().top-100
-                }, 1000);
-              },
-              classes: secondButtonClass
-            }
-        ];
+      var buttons = [];
+      
+      if (prevStepSelector) {
+        buttons.push({
+          text: 'Back',
+          action: function() {
+              // Move on to the next step.
+              tour.back();
+              // Now, scroll to the next step.
+              $('html, body').animate({
+                scrollTop: $(prevStepSelector).offset().top-100
+              }, 1000);
+            },
+          classes: 'shepherd-button-secondary'
+        });
+      }
+      if (isLastStep) { 
+        buttons.push({
+          text: 'Done',
+          action: function() {
+              // Move on to the next step.
+              tour.complete();
+              // Now, scroll to the next step.
+              $('html, body').animate({
+                scrollTop: $(nextStepSelector).offset().top-100
+              }, 1000);
+            },
+          classes: 'shepherd-button-complete'
+        });
+      }
+      else {
+        buttons.push({
+          text: 'Next',
+          action: function() {
+              // Move on to the next step.
+              tour.next();
+              // Now, scroll to the next step.
+              $('html, body').animate({
+                scrollTop: $(nextStepSelector).offset().top-100
+              }, 1000);
+            },
+          classes: 'shepherd-button-primary'
+        });
+      }
+
+      return buttons;
     }
   }}
 }(jQuery));
